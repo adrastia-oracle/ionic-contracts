17abbb254f13be1cd7173696ac9a38d6560bb601
Jourdan Dunkley <chasejamaica@yahoo.com>
2024-07-15 22:15:42 -0500

    prettier

--- contracts/compound/CToken.sol
+++ contracts/compound/CToken.sol
@@ -450,7 +450,11 @@ abstract contract CErc20 is CTokenSecondExtensionBase, TokenErrorReporter, Expon
    * @param redeemAmountIn The number of underlying tokens to receive from redeeming cTokens (only one of redeemTokensIn or redeemAmountIn may be non-zero)
    * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
    */
-  function redeemFresh(address redeemer, uint256 redeemTokensIn, uint256 redeemAmountIn) internal returns (uint256) {
+  function redeemFresh(
+    address redeemer,
+    uint256 redeemTokensIn,
+    uint256 redeemAmountIn
+  ) internal returns (uint256) {
     require(redeemTokensIn == 0 || redeemAmountIn == 0, "!redeem tokens or amount");
 
     RedeemLocalVars memory vars;
@@ -675,10 +679,11 @@ abstract contract CErc20 is CTokenSecondExtensionBase, TokenErrorReporter, Expon
    * @param repayAmount The amount to repay
    * @return (uint, uint) An error code (0=success, otherwise a failure, see ErrorReporter.sol), and the actual repayment amount.
    */
-  function repayBorrowBehalfInternal(
-    address borrower,
-    uint256 repayAmount
-  ) internal nonReentrant(false) returns (uint256, uint256) {
+  function repayBorrowBehalfInternal(address borrower, uint256 repayAmount)
+    internal
+    nonReentrant(false)
+    returns (uint256, uint256)
+  {
     asCTokenExtension().accrueInterest();
     // repayBorrowFresh emits repay-borrow-specific logs on errors, so we don't need to
     return repayBorrowFresh(msg.sender, borrower, repayAmount);
@@ -702,7 +707,11 @@ abstract contract CErc20 is CTokenSecondExtensionBase, TokenErrorReporter, Expon
    * @param repayAmount the amount of undelrying tokens being returned
    * @return (uint, uint) An error code (0=success, otherwise a failure, see ErrorReporter.sol), and the actual repayment amount.
    */
-  function repayBorrowFresh(address payer, address borrower, uint256 repayAmount) internal returns (uint256, uint256) {
+  function repayBorrowFresh(
+    address payer,
+    address borrower,
+    uint256 repayAmount
+  ) internal returns (uint256, uint256) {
     /* Fail if repayBorrow not allowed */
     uint256 allowed = comptroller.repayBorrowAllowed(address(this), payer, borrower, repayAmount);
     if (allowed != 0) {
