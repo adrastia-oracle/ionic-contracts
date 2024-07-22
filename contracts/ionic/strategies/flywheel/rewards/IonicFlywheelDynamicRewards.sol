bf99e80ee1aec674ff175078b5f3f696f11ccbc8
antisaa <antesosa95@gmail.com>
2024-07-22 12:25:37 +0200

    Update Ionic flywheel dynamic rewards

--- contracts/ionic/strategies/flywheel/rewards/IonicFlywheelDynamicRewards.sol
+++ contracts/ionic/strategies/flywheel/rewards/IonicFlywheelDynamicRewards.sol
@@ -5,28 +5,26 @@ import { FlywheelDynamicRewards } from "./FlywheelDynamicRewards.sol";
 import { IonicFlywheelCore } from "../IonicFlywheelCore.sol";
 import { SafeTransferLib, ERC20 } from "solmate/utils/SafeTransferLib.sol";
 
-interface ICERC20 {
-  function plugin() external returns (address);
-}
-
-interface IPlugin {
-  function claimRewards() external;
-}
-
 contract IonicFlywheelDynamicRewards is FlywheelDynamicRewards {
-  using SafeTransferLib for ERC20;
+    using SafeTransferLib for ERC20;
 
-  constructor(IonicFlywheelCore _flywheel, uint32 _cycleLength) FlywheelDynamicRewards(_flywheel, _cycleLength) {}
+    constructor(IonicFlywheelCore _flywheel, uint32 _cycleLength)
+        FlywheelDynamicRewards(_flywheel, _cycleLength)
+    {}
 
-  function getNextCycleRewards(ERC20 strategy) internal override returns (uint192) {
-    // make it work for both pulled (claimed) and pushed (transferred some other way) rewards
-    try ICERC20(address(strategy)).plugin() returns (address plugin) {
-      try IPlugin(plugin).claimRewards() {} catch {}
-    } catch {}
-    uint256 rewardAmount = rewardToken.balanceOf(address(strategy));
-    if (rewardAmount != 0) {
-      rewardToken.safeTransferFrom(address(strategy), address(this), rewardAmount);
+    function getNextCycleRewards(ERC20 strategy)
+        internal
+        override
+        returns (uint192)
+    {
+        uint256 rewardAmount = rewardToken.balanceOf(address(strategy));
+        if (rewardAmount != 0) {
+            rewardToken.safeTransferFrom(
+                address(strategy),
+                address(this),
+                rewardAmount
+            );
+        }
+        return uint192(rewardAmount);
     }
-    return uint192(rewardAmount);
-  }
 }
